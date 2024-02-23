e0150ade31796cf045eb7b1d8daa883fa02e37ef
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-02-23 16:23:06 -0600

    Reproduce issue with mysql-any-password
    https://dev.mysql.com/doc/refman/8.0/en/pam-pluggable-authentication.html#pam-pluggable-authentication-debugging

--- tests/test.sh
+++ tests/test.sh
@@ -4,8 +4,7 @@
 # ./tests/test.sh '<demouser_password>'
 
 docker compose up --build -d --wait percona
-docker compose exec percona mysql -hlocalhost -uroot -ppassword -e "CREATE USER 'demouser'@'%' IDENTIFIED WITH auth_pam AS 'oidc';"
-docker compose exec percona mysql -hlocalhost -uroot -ppassword -e "SHOW PLUGINS;" | grep auth_pam
+docker compose exec percona mysql -hlocalhost -uroot -ppassword -e "CREATE USER 'demouser'@'%' IDENTIFIED WITH auth_pam AS 'mysql-any-password';"
 docker compose exec percona mysql -hlocalhost -udemouser -p"$1" --enable-cleartext-plugin -e "SELECT 1;" || echo "Failed to authenticate with real password"
 docker compose exec percona mysql -hlocalhost -udemouser -p'bogus_password' --enable-cleartext-plugin -e "SELECT 1;" || echo "Failed to authenticate for bogus password"
 sleep 3
